8cfb1f75105b60c3340fe315a4dce97a66a392e6
Taylor Lookabaugh <jesus.christ.i.love@gmail.com>
2011-09-13 11:33:51 -0700

    add second trig code using for loop

---
+++ src/trig_table_b.c
@@ -0,0 +1,30 @@
+/*
+Submitter: Taylor Lookabaugh
+file: trig_table_a.c
+program #7
+purpose: to print trig table with answers to every 15 degrees
+        from 0-360 degrees in sine and cosine.
+apply: use while loop.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (void)
+{
+    int angle_degrees=0;
+    double angle_radians_sin, angle_radians_cos;
+
+    printf("\t Table of Trig values \n");
+    printf("  angle(deg.)      sin            cos \n");
+    for (angle_degrees = 0; angle_degrees <= 360; angle_degrees += 15)
+    {
+        angle_radians_sin = sin(angle_degrees*M_PI/180);
+        angle_radians_cos = cos(angle_degrees*M_PI/180);
+        printf("%6i %18.4f %14.4f \n", angle_degrees, angle_radians_sin, angle_radians_cos);
+    }
+
+    system("PAUSE");
+    return(0);
+}
