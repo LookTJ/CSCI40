e5655a10a42986d271189098b5a3aaa990171f37
Taylor Lookabaugh <jesus.christ.i.love@gmail.com>
2011-09-15 00:28:36 -0700

    add factorial.c

---
+++ src/factorial.c
@@ -0,0 +1,38 @@
+/*
+Submitter: Taylor Lookabaugh
+file: factorial.c
+program #4
+purpose: to calculate factorial
+apply: using while loop
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+    int x, i, f=1;
+
+    /* read integer input from keyboard with limits */
+    do
+    {
+    printf("Enter a positive value for x > ");
+    scanf("%i", &x);
+    i = x;
+    if(x>12)
+    {
+        printf("Cannot proceed due to limitation, \n"
+                "Please retry. \n");
+    }
+    } while (x<0 || x>12);
+
+    /* calculate factorial of an integer */
+    while(x>0)
+    {
+        f *= x;
+        x--;
+    }
+    printf("integer = %i; factorial = %i \n", i, f);
+
+    return(0);
+}
